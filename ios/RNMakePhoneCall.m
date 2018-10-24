bdd8842c249472ee008a3d106fdf49e16b72730e
Edgar Lopez <inge.edgarlopez@gmail.com>
2018-10-24 12:08:35 -0700

    Remove swift code :(

--- ios/RNMakePhoneCall.m
+++ ios/RNMakePhoneCall.m
@@ -1,5 +1,6 @@
 
 #import "RNMakePhoneCall.h"
+#import <UIKit/UIKit.h>
 
 @implementation RNMakePhoneCall
 
@@ -8,6 +9,27 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
-RCT_EXTERN_METHOD(makeCall: (NSString *)number)
+
+RCT_EXPORT_METHOD(makeCall: (NSString *)number)
+{
+    UIApplication *application = [UIApplication sharedApplication];
+    
+    NSURL *URL = [NSURL URLWithString:[@"tel://" stringByAppendingString:number]];
+    
+    if([[UIDevice currentDevice].systemVersion floatValue] >= 10.0){
+        if ([application respondsToSelector:@selector(openURL:options:completionHandler:)]) {
+            [application openURL:URL options:@{}
+               completionHandler:^(BOOL success) {
+               }];
+        } else {
+            [application openURL:URL];
+        }
+    } else {
+        bool can = [application canOpenURL:URL];
+        if(can) {
+            [application openURL:URL];
+        }
+    }
+}
 @end
   
